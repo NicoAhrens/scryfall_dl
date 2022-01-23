cda6854b16df8f38900df3dcdc8941a7f6af625c
NicoAhrens <90962381+NicoAhrens@users.noreply.github.com>
2022-01-23 13:00:49 +0100

    Delete scryfall_pic_dl.py

--- scryfall_pic_dl.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-def install_scrython_package():
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "scrython"]
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "asyncio"]
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "aiohttp"]
