ee4b1990ee9fb7fc364c9ab0e37199566369e6f7
NicoAhrens <nico_ahrens89@web.de>
2022-01-24 16:04:13 +0100

    correctet typos in README.md and added comments to the code in main.py

--- README.md
+++ README.md
@@ -1,32 +1,32 @@
-### Downloader for Scryfall Magic The Gathering Pictures ###
+# MTG Card Picture Downloader for Scryfall API 
 
 * only for English Card names 
 
-INPUT: List of Magic Card Names without Numbers. Each row only has one Name of 
-card. 
+## WHAT DOES THE SCRIPT
+With this Python script you can download a set of MTG card pictures with a deck list from [Scryfall.com](https://scryfall.com/)
 
-OUTPUT: .png files of the Magic Cards from the Input list
+## INPUT: 
+List of MTG card names without numbers. Each row only has one name of card with no trailing whitespace.
 
-Dependencies: 
-Python3
+## OUTPUT: 
+`<card_name>.png ` files of the Magic Cards from the Input list
 
-HOW TO USE:
+## HOW TO USE:
 
 1. Create a .txt file with every name of the magic cards of which you want 
 to download the pictures from.
 2. Therefore put only one name in each row and without numbers like the 
-following example: 
+following example. Be aware of trailing whitespace.
 
-file_name.txt: 
-
-Sol Ring <br />
-Vindicate <br />
-Black Lotus <br />
+`<name>.txt`: 
+Sol Ring<br />
+Vindicate<br />
+Black Lotus<br />
 [...]
 
-3. Put the file_name.txt file in the same directory as the main.py file
-4. First run the terminal in the same path as the main.py. Or run it with the full path name. Run the python script in the shell with "python3 main.py".
-5. The programm asks you for the name of the .txt file with magic cards you want to download from scryfall.com. 
-Type only the filename without the filetype (without ".txt")
-6. Wait for the Download to finish. 
-7. Pictures can be found in folder with the same name as the .txt file.  
+3. Put the `<name>.txt` file in the same directory as the `main.py` file
+4. First run the terminal in the same path as the `main.py.` Or run it with the full path name. Run the python script in the shell with `$ python3 main.py`.
+5. The programm asks you for the name of the `<name>.txt` file with magic cards you want to download from scryfall.com. 
+Type only the filename without the filetype (without ".txt") into the command prompt.
+6. Wait for the Download to finish.
+7. Pictures can be found in folder with the same name as the .txt file.
--- main.py
+++ main.py
@@ -7,47 +7,59 @@ import os
 from requests import get
 # reading out json files for finding the right card in the list
 from json import loads
-# for saving the found card in the end to a png file
+# for saving the found card at the end to a png file
 from shutil import copyfileobj
 
 from pathlib import Path
 
-# creating neccessary variables
-# save_folder is the current workind directory
+# Creating neccessary variables
+# save_folder is the current workind directory, for easier use of path in
+# following code
 save_folder = os.path.join(os.getcwd())
-# Input from the User for reading the right file
+# Input from the User. read_file saves the name of the while, which the user
+# wants to put into the script
 read_file = input('Type in the name of the file without the ".txt" (case sensitive)\n> ')
-# creating the file name for reading it out
+# Creating the file name with the .txt extension for reading it out
+# with splitlines()
 read_file_type = read_file + '.txt'
+# Creates a folder with the name of the given list to save the pictures of the
+# cards
 if not os.path.isdir(read_file):
     os.mkdir(read_file)
-# reads out every line of the .txt file and puts them into a list
+# Reads out every line of the .txt file and puts them into a list
 with open(read_file_type) as f:
     lines = f.read().splitlines()
 
-
+# Iteration over every card from the list and searching for it on the scryfall
+# site.
 for image in range(len(lines)):
+    # time.sleep to not overload the server of the scryfall with requests
     time.sleep(0.1)
     # Load the card data from Scryfall
     card = loads(get(f"https://api.scryfall.com/cards/search?q={lines[image]}").text)
-    # print(card)
-    # Get the image URL
-    # print(lines[image])
+    # checking if the first card which is found with the query, if its the right
+    # one. For e.g. for the query "Black Lotus", the first card in the output
+    # is "Blacker Lotus" and the second one is "Black Lotus"
     for image_id in range(len(card['data'])):
-        # print(image_id)
         card_path_name = card['data'][image_id]['name']
         if card_path_name == lines[image]:
             img_url = card['data'][image_id]['image_uris']['png']
 
-    # creates a f
+    # creates a save name for the file within the created savefolder with
+    # save_folder and read_file. The filename is given with the name of the
+    # searched card nam with lines[image]
     file_save_name = os.path.join(save_folder, read_file, lines[image])
 
     # saving the image into the right folder
-
+    # checks if the card allready exists and if not saves it with the given
+    # file_save_name as an .png
     if Path(f'{file_save_name}.png').exists():
-        # file exists
+        # gives out a statement for the user if the given card is allready
+        # downloaded
         print(f"Allready downloaded: {card_path_name}")
     else:
         with open(f'{file_save_name}.png', 'wb') as out_file:
             copyfileobj(get(img_url, stream=True).raw, out_file)
+            # gives out a statement for the user if the download is finished
+            # for the given card
             print(f"Download complete for: {card_path_name}")
